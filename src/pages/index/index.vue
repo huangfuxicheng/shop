6d5091504994da6d6166eef02c6f2424e2c4c7a1
middlings <middlings@qq.com>
2024-01-06 23:29:23 +0800

    feat: 首页分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@ import { getHomeBanner } from '@/services/home'
 import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
@@ -18,9 +19,12 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>

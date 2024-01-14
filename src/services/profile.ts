1a7ab4c08d00668fdc7280eeb0c84bb175cd9fb4
middlings <middlings@qq.com>
2024-01-14 23:53:24 +0800

    feat: 修改个人信息接口

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,5 +1,5 @@
 import { http } from '@/utils/http'
-import type { ProfileDetail } from '@/types/member'
+import type { ProfileDetail, ProfileParams } from '@/types/member'
 
 /**
  * 获取个人信息
@@ -8,5 +8,13 @@ export const getMemberProfile = () => {
   return http<ProfileDetail>({
     method: 'GET',
     url: '/member/profile',
+  })
+}
+
+export const putMemberProfile = (data: ProfileParams) => {
+  return http<ProfileDetail>({
+    method: 'PUT',
+    url: '/member/profile',
+    data,
   })
 }
--- src/types/member.d.ts
+++ src/types/member.d.ts
@@ -27,3 +27,16 @@ export type ProfileDetail = BaseProfile & {
 }
 
 export type Gender = '男' | '女'
+
+/** 个人信息 修改请求体参数 */
+export type ProfileParams = Pick<
+  ProfileDetail,
+  'nickname' | 'gender' | 'birthday' | 'profession'
+> & {
+  /** 省份编码 */
+  provinceCode?: string
+  /** 城市编码 */
+  cityCode?: string
+  /** 区/县编码 */
+  countyCode?: string
+}

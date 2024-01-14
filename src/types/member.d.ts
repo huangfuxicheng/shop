3537069b9dbcc562066db48916b1a85f51d5b4c1
middlings <middlings@qq.com>
2024-01-14 23:00:45 +0800

    feat: 个人中心页面信息展示

--- src/types/member.d.ts
+++ src/types/member.d.ts
@@ -1,5 +1,6 @@
-/** 小程序登录 登录用户信息 */
-export type LoginResult = {
+import { extend } from '@dcloudio/uni-app'
+
+type BaseProfile = {
   /** 用户ID */
   id: number
   /** 头像  */
@@ -8,8 +9,21 @@ export type LoginResult = {
   account: string
   /** 昵称 */
   nickname?: string
+}
+
+/** 小程序登录 登录用户信息 */
+export type LoginResult = BaseProfile & {
   /** 手机号 */
   mobile: string
   /** 登录凭证 */
   token: string
 }
+
+export type ProfileDetail = BaseProfile & {
+  birthday: string
+  fullLocation: string
+  gender: string
+  profession: string
+}
+
+export type Gender = '男' | '女'

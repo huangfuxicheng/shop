5a2cd0e1fa635d3dbd84a382cb0a9709db67d25d
middlings <middlings@qq.com>
2024-01-19 21:44:01 +0800

    feat: 删除单品

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-import { getMemberCart } from '@/services/cart'
+import { deleteMemberCartAPI, getMemberCart } from '@/services/cart'
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { CartItem } from '@/types/cart'
@@ -18,6 +18,22 @@ onShow(() => {
     getMemberCartData()
   }
 })
+
+// 点击删除按钮
+const onDeleteCart = (skuId: string) => {
+  // 弹窗二次确认
+  uni.showModal({
+    content: '是否删除',
+    success: async (res) => {
+      if (res.confirm) {
+        // 后端删除单品
+        await deleteMemberCartAPI({ ids: [skuId] })
+        // 重新获取列表
+        getMemberCartData()
+      }
+    },
+  })
+}
 </script>
 
 <template>
@@ -65,7 +81,7 @@ onShow(() => {
             <!-- 右侧删除按钮 -->
             <template #right>
               <view class="cart-swipe-right">
-                <button class="button delete-button">删除</button>
+                <button @tap="onDeleteCart(item.skuId)" class="button delete-button">删除</button>
               </view>
             </template>
           </uni-swipe-action-item>
